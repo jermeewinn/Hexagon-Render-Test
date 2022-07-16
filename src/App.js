7f056afdfeb522b0cb3b478f236b7dc3c71b951d
Jeremy Huynh <jeahuynh18@gmail.com>
2022-07-15 23:29:08 -0700

    imported Navigation object into App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from '../src/components/Navigation';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navigation />
     </div>
   );
 }
