20131f0e7801f1426b988533aecde8b3631b1067
Jeremy Huynh <jeahuynh18@gmail.com>
2022-07-15 23:37:16 -0700

    added helmet dependency to components

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 function Home() {
-
+    return(
+        <section>
+            <div>
+                <Helmet>
+                    <title>Hexagon Render Test | Home</title>
+                </Helmet>
+            </div>
+        </section>
+    )
 }
 
 export default Home;
--- src/components/Other/index.js
+++ src/components/Other/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 function Other() {
-
+    return(
+        <section>
+            <div>
+                <Helmet>
+                    <title>Hexagon Render Test | Other</title>
+                </Helmet>
+            </div>
+        </section>
+    )
 }
 
 export default Other;
