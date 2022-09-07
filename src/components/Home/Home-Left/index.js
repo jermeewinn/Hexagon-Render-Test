612efa33a0f5b6cbb6f32c207dfab7f4a4a130c6
Jeremy Huynh <jeahuynh18@gmail.com>
2022-09-06 21:24:45 -0700

    sanity check

--- src/components/Home/Home-Left/index.js
+++ src/components/Home/Home-Left/index.js
@@ -2,6 +2,28 @@ import React from 'react';
 import Hexagon from 'react-hexagon';
 
 const HomeLeft = () => {
+    const [services] = useState([
+        {
+            name: 'Resume Review',
+            description: 'Have one of our consultants take a look at your resume, and work with you in transforming it into a key that will open doors for your future job hunt. We optimize for ATS so that you can get your foot in more doors.',
+            src: 'Resume.jpg',
+        },
+        {
+            name: 'LinkedIn Revision',
+            description: 'The future of job hunting is online, and Linkedin is a major part of that. We will help you create and configure your LinkedIn profile so that you can start job hunting quickly and efficiently.',
+        },
+        {
+            name: 'Career Consulting',
+            description: 'If the job application asks for a cover letter, submit one. We can work with you in going over key aspects on formatting, composition, and delivery of your cover letter.',
+        },
+        {
+            name: 'Interview Prep',
+            description: "Got something on the books? That's great! We'll give you the interview prep you need so that you don't go into the interview lost. We will give you a mock interview so that you can mentally engage",
+        },
+        {
+            name: 'Cover Letter'
+        }
+    ])
     return(
         <div id='hex-ring' className='hex-left'>
             <div id='root' className='hex-row d-flex justify-content-center'>
